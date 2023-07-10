cc1313ad19e069ec57b4ef9cd830a12c2977c948
Aleem Rehmtulla <1rehmtullaal2@gmail.com>
2023-07-09 21:38:38 -0400

    remove await for hitpage

--- pages/[user].tsx
+++ pages/[user].tsx
@@ -52,7 +52,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   }
 
   const BASE_URL = getBaseURL()
-  await fetch(BASE_URL + '/api/analytics/hitpage', {
+  fetch(BASE_URL + '/api/analytics/hitpage', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
