0899af982c597534baf209ec9b9312fad6b44a3c
Aleem Rehmtulla <1rehmtullaal2@gmail.com>
2023-07-09 22:49:55 -0400

    Create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,36 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.G_TAG}`} />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${process.env.G_TAG}');
+          `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
