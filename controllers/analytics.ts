2f2a588ca2e9728d8944c0fbc9c536596cc3fbc1
Aleem Rehmtulla <1rehmtullaal2@gmail.com>
2023-07-11 23:08:03 -0400

    filter empty referrers in top traffic sources

--- controllers/analytics.ts
+++ controllers/analytics.ts
@@ -191,7 +191,10 @@ export const GetTrafficSources = async (kyteId: string): Promise<GetTrafficSourc
         count: trafficSources._count.referrer,
       }
     })
-    .filter((trafficSource) => trafficSource.referrer !== null) as GetTrafficSourcesReturnData
+    .filter((trafficSource) => trafficSource.referrer !== null)
+    .filter((trafficSource) => trafficSource.referrer !== '')
+    .sort((a, b) => b.count - a.count)
+    .slice(0, 5) as GetTrafficSourcesReturnData
 
   return sanitizedTrafficSources
 }
