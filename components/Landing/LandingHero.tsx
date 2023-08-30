615ed4dee2b63a77fca35122117e90a7a5a1a225
Aleem Rehmtulla <1rehmtullaal2@gmail.com>
2023-08-30 19:13:54 -0400

    copy change

--- components/Landing/LandingHero.tsx
+++ components/Landing/LandingHero.tsx
@@ -3,8 +3,8 @@ const LandingHero = () => {
   return (
     <VStack spacing={4} align="center">
       <VStack spacing={0} align="center">
-        <Heading fontSize={{ base: '3xl', md: '5xl', lg: '7xl' }}>A Simple Link-In-Bio.</Heading>
-        <Heading fontSize={{ base: '3xl', md: '5xl', lg: '7xl' }}>But Free and Opensource.</Heading>
+        <Heading fontSize={{ base: '3xl', md: '5xl', lg: '7xl' }}>A Simple Link-In-Bio</Heading>
+        <Heading fontSize={{ base: '3xl', md: '5xl', lg: '7xl' }}>Free and Opensource</Heading>
       </VStack>
 
       <Text
