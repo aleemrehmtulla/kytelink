f9810ac8bb78431207aa3dc0a1517f3c722eb849
Aleem Rehmtulla <1rehmtullaal2@gmail.com>
2023-09-26 00:24:06 -0400

    prisma: add migration file

---
+++ prisma/migrations/20230926041917_add_metadata/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "KyteDraft" ADD COLUMN     "seoDescription" TEXT,
+ADD COLUMN     "seoTitle" TEXT;
+
+-- AlterTable
+ALTER TABLE "KyteProd" ADD COLUMN     "seoDescription" TEXT,
+ADD COLUMN     "seoTitle" TEXT;
